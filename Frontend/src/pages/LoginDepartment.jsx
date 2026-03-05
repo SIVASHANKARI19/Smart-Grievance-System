1539d2888370bfa319e3af03109e181e34de0095
SIVASHANKARI19 <151622873+SIVASHANKARI19@users.noreply.github.com>
2026-03-05 11:03:24 +0530

    updated loginDepartment

--- Frontend/src/pages/LoginDepartment.jsx
+++ Frontend/src/pages/LoginDepartment.jsx
@@ -8,6 +8,7 @@ import { Button } from "../components/Button";
 import { Card } from "../components/Card";
 import { AlertCircle, Building2 } from "lucide-react";
 
+
 export const LoginDepartment = () => {
   const navigate = useNavigate();
   const { login, isAuthenticated, user } = useAuth();
