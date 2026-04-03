722186150a5ec4e9e36ea115c32b33ce7340b587
SIVASHANKARI19 <151622873+SIVASHANKARI19@users.noreply.github.com>
2026-04-04 01:08:39 +0530

    added frontend url for cors error to backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 const MONGOURL = process.env.MONGO_URI;
 
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 // Increase the JSON payload limit to 10MB to support Base64 image uploads
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
